38afc8aefee73f9945fc4cae6cb468468ef18a81
Jaedon Shin <jaedon.shin@gmail.com>
2021-08-20 12:42:05 +0900

    Add formula for gcc-arm-linux-gnueabihf

---
+++ Formula/gcc-arm-linux-gnueabihf.rb
@@ -0,0 +1,15 @@
+class GccArmLinuxGnueabihf < Formula
+  desc "Pre-built ARM/Linux cross compilers for macOS"
+  homepage "https://github.com/jaedon/gcc-arm-linux-gnueabihf"
+  url "https://github.com/jaedon/gcc-arm-linux-gnueabihf/releases/download/gcc-11-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--11.2.0.big_sur.bottle.tar.gz"
+  sha256 "41643eff21921c273f7f2d680c7780fabcabf021273b23cc93e30c2fd0560263"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+
+  def install
+    prefix.install Dir["11.2.0/*"]
+  end
+
+  test do
+    system bin/"arm-linux-gnueabihf-gcc", "-v"
+  end
+end
