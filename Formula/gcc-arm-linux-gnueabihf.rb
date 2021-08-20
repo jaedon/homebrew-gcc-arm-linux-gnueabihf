ad5d1db8bb8eb270ff4e5467a5151d9b3c33d9e6
Jaedon Shin <jaedon.shin@gmail.com>
2021-08-20 12:57:31 +0900

    Rename repository for Homebrew Taps

--- Formula/gcc-arm-linux-gnueabihf.rb
+++ Formula/gcc-arm-linux-gnueabihf.rb
@@ -1,7 +1,7 @@
 class GccArmLinuxGnueabihf < Formula
   desc "Pre-built ARM/Linux cross compilers for macOS"
-  homepage "https://github.com/jaedon/gcc-arm-linux-gnueabihf"
-  url "https://github.com/jaedon/gcc-arm-linux-gnueabihf/releases/download/gcc-11-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--11.2.0.big_sur.bottle.tar.gz"
+  homepage "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf"
+  url "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf/releases/download/gcc-11-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--11.2.0.big_sur.bottle.tar.gz"
   sha256 "41643eff21921c273f7f2d680c7780fabcabf021273b23cc93e30c2fd0560263"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
--- Formula/gcc-arm-linux-gnueabihf@6.rb
+++ Formula/gcc-arm-linux-gnueabihf@6.rb
@@ -1,7 +1,7 @@
 class GccArmLinuxGnueabihfAT6 < Formula
   desc "Pre-built ARM/Linux cross compilers for macOS"
-  homepage "https://github.com/jaedon/gcc-arm-linux-gnueabihf"
-  url "https://github.com/jaedon/gcc-arm-linux-gnueabihf/releases/download/gcc-6-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--6.5.0.big_sur.bottle.tar.gz"
+  homepage "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf"
+  url "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf/releases/download/gcc-6-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--6.5.0.big_sur.bottle.tar.gz"
   sha256 "cd32c1b214cc54a76ed65c010d191d7ae633db03485353555c543a46c0a96246"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
