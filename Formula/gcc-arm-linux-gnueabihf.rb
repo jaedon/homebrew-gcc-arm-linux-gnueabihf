19b5eda94bcdf04dad0dc8c73c59fdc77474d7f6
Jaedon Shin <jaedon.shin@gmail.com>
2022-05-19 16:27:20 +0900

    Update formula for gcc-arm-linux-gnueabihf

--- Formula/gcc-arm-linux-gnueabihf.rb
+++ Formula/gcc-arm-linux-gnueabihf.rb
@@ -1,14 +1,14 @@
 class GccArmLinuxGnueabihf < Formula
   desc "Pre-built ARM/Linux cross compilers for macOS"
   homepage "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf"
-  url "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf/releases/download/gcc-11-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--11.2.0.big_sur.bottle.tar.gz"
-  sha256 "41643eff21921c273f7f2d680c7780fabcabf021273b23cc93e30c2fd0560263"
+  url "https://github.com/jaedon/homebrew-gcc-arm-linux-gnueabihf/releases/download/gcc-12-arm-linux-gnueabihf/gcc-arm-linux-gnueabihf--12.1.0.big_sur.bottle.tar.gz"
+  sha256 "9781dab02d3dc6147c12025bc8123c7e06aee8224a69893c9cadc1e1589b7ca4"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   keg_only :versioned_formula
 
   def install
-    prefix.install Dir["11.2.0/*"]
+    prefix.install Dir["12.1.0/*"]
   end
 
   test do
